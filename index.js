f714b6d9463753f5a406b30c5992f3169977b098
Kelley Shackelford <kelleyalexis@yahoo.com>
2024-03-21 11:46:17 -0400

    Used array finder method

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(records) { 
+    const winRecord = records.find(record => record.result === "W");
+    if (winRecord) {
+        return winRecord.year;
+    }
+}
+console.log(records);
